e13b3db64aef25d0ee94345c9c192880c3945d8d
KWhiteC <54796720+KWhiteC@users.noreply.github.com>
2022-08-17 02:07:24 +0900

    Add files via upload

---
+++ LeeSeunghyun/Level1/Dart.py
@@ -0,0 +1,45 @@
+def solution(dartResult):
+
+    n = ''
+
+    score = []
+
+    for i in dartResult:
+
+        if i.isnumeric():
+
+        #isnumeric()  : 문자인지 아닌지 판별해주는 method
+
+            n += i
+
+        elif i == 'S':
+
+            n = int(n) ** 1
+            score.append(n)
+            n = ''
+
+        elif i == 'D':
+
+            n = int(n) ** 2
+            score.append(n)
+            n = ''
+
+        elif i == 'T':
+
+            n = int(n) ** 3
+            score.append(n)
+            n = ''
+
+        elif i == '*':
+
+            if len(score) > 1:
+                score[-2] = score[-2] * 2
+                score[-1] = score[-1] * 2
+            else:
+                score[-1] = score[-1] * 2
+
+        elif i == '#':
+
+            score[-1] = score[-1] * -1
+
+    return sum(score)
