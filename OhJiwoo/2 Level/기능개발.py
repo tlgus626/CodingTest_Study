dab7b52a9d5dbac94e84726021bf2539c1afd790
woooyen <110808374+woooyen@users.noreply.github.com>
2022-08-16 22:29:36 +0900

    Update 기능개발.py

---
+++ OhJiwoo/2 Level/기능개발.py
@@ -0,0 +1,22 @@
+from collections import deque
+import math
+def solution(progresses, speeds):
+    answer=[]
+    p=[]
+    q=deque()
+    #각 작업이 걸리는 시간을 구함 
+    for i in range(len(progresses)) :
+            p=math.ceil((100-progresses[i])/speeds[i])
+            q.append(p)
+    a=q[0]
+    count=0
+    while q:
+        qq=q.popleft()
+        if a >= qq:
+            count +=1
+        else: 
+            answer.append(count)
+            a=max(a,qq)
+            count=1
+    answer.append(count)             
+    return answer
