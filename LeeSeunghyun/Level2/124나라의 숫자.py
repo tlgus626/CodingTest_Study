07012a684a1b6b41bd90cc0627f6ee8369e81016
KWhiteC <54796720+KWhiteC@users.noreply.github.com>
2022-08-22 12:33:23 +0900

    Update 124나라의 숫자.py

--- LeeSeunghyun/Level2/124나라의 숫자.py
+++ LeeSeunghyun/Level2/124나라의 숫자.py
@@ -1,29 +1,24 @@
 def solution(n):
-    x=n-1 #찾은 규칙에 따라
-    answer=''
-    while True: #3진법에서 모티브.
-        x,y=divmod(x,3) # 몫과 나머지 반환.
-        if y==0:
-            answer+='1'
-        elif y==1:
-            answer+='2'
+    
+    x = n-1 #찾은 규칙에 따라
+    answer = ''
+    
+    while True : #3진법에서 모티브.
+        
+        x, y = divmod(x,3) # 몫과 나머지 반환.
+        
+        if y == 0 :
+            answer += '1'
+        
+        elif y == 1 :
+            answer += '2'
+        
         else:
-            answer+='4'
-        if x==0:#이게 loop 탈출의 결정적 기준.
+            answer += '4'
+        
+        if x == 0 : #이게 loop 탈출의 결정적 기준.
             break
-        x=x-1
+        
+        x = x-1
     return answer[::-1] #나머지에 따라 저장된 값을 역순으로 출력 : 진법변환처럼
 
-
-### 모범답안 ###
-def change124(n):
-    num = ['1','2','4']
-    answer = ""
-
-
-    while n > 0:
-        n -= 1
-        answer = num[n % 3] + answer
-        n //= 3
-
-    return answer
