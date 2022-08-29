dd45b6abbd836872b8d24c939bacafc8a4a73873
KWhiteC <54796720+KWhiteC@users.noreply.github.com>
2022-08-29 14:24:52 +0900

    Update 문자열 압축.py

--- LeeSeunghyun/Level2/문자열 압축.py
+++ LeeSeunghyun/Level2/문자열 압축.py
@@ -1,26 +1,41 @@
 def solution(s):
     answer = len(s)
     
-    # 완전 탐색
-    for step in range(1, len(s)//2+1):
-        count = 1 # count 변수 사용
-        compressed = 0 # 길이만 담는 변수 설정
-        prev = s[:step]
+    ### 완전 탐색 ###
+    
+    ## step수 기준으로 outer 돌림 ###
+    for step in range(1, len(s)+1):
+        compressed = ''
+        arr = []
         
-        for start in range(step, len(s)+step, step):
-            
-            curr = s[start:start+step]
-            
-            if curr == prev:
-                
-                count += 1 # 문자열 대신 count만 증가
-            
-            else:
-                
-                compressed += len(str(count)+prev) if count > 1 else len(prev)
-                count = 1 # 배열 대신 count = 1로 리셋
-                prev = curr
-        
-        answer = min(answer, compressed)
+        #arr이 비어있다면
+        ## 해당 step수에 맞게 문자열 slice ##
+        for start in range(0, len(s), step):
+            end = start + step
+            sliced = s[start:end]
 
+            
+            if start == 0:
+               arr.append(sliced)
+                
+            else:
+                if arr[-1] != sliced: # 중복 문자열을 체크하는 것이기 때문에 0이어도 무관
+                    if len(arr) == 1:
+                        compressed += f'{arr[-1]}'
+                    else:
+                        compressed += f'{len(arr)}{arr[-1]}'
+                    arr = []
+                
+                arr.append(sliced)
+        
+        #arr이 비어있지 않다면
+        if arr: 
+            if len(arr) == 1:
+                compressed += f'{arr[-1]}'
+            else:
+                compressed += f'{len(arr)}{arr[-1]}'
+        
+        
+        answer = min([len(compressed), answer])
+    
     return answer
