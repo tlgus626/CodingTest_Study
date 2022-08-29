fd2be014f62ca33c59de7087028035474443844b
KWhiteC <54796720+KWhiteC@users.noreply.github.com>
2022-08-29 12:19:22 +0900

    Update 문자열 압축.py

--- LeeSeunghyun/Level2/문자열 압축.py
+++ LeeSeunghyun/Level2/문자열 압축.py
@@ -8,17 +8,18 @@ def solution(s):
         prev = s[:step]
         
         for start in range(step, len(s)+step, step):
+            
             curr = s[start:start+step]
             
             if curr == prev:
                 
-            count += 1 # 배열에 문자열을 실제로 넣어주는 대신 count만 증가
+                count += 1 # 배열에 문자열을 실제로 넣어주는 대신 count만 증가
             
             else:
                 
-            compressed += len(str(count)+prev) if count > 1 else len(prev)
-            count = 1 # 배열을 비워주는 대신 count = 1로 리셋
-            prev = curr
+                compressed += len(str(count)+prev) if count > 1 else len(prev)
+                count = 1 # 배열을 비워주는 대신 count = 1로 리셋
+                prev = curr
         
         answer = min(answer, compressed)
 
