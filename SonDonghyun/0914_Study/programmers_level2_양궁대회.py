b66d0a3dcf61e9e6928ef3f7df9c15d9ca28e922
thon <stat.thon@gmail.com>
2022-09-10 20:30:05 +0900

    양궁대회 코드정리

--- SonDonghyun/0914_Study/programmers_level2_양궁대회.py
+++ SonDonghyun/0914_Study/programmers_level2_양궁대회.py
@@ -1,12 +1,9 @@
 def solution(n, info):
 
-    # 이 함수는 같은 점수 차이로 이긴다면 더 작은 점수가 많은 배열을 return하기 위한 함수
-    def lower_score_comp(a, b):
+    def a_or_b(a, b): # 같은 점수 차이로 이긴다면 작은 점수가 많은 배열을 return하기 위한 함수
         for i in range(10, -1, -1):
-            if a[i] == b[i]: pass
-            elif a[i] > b[i]: return a
+            if a[i] > b[i]: return a
             elif a[i] < b[i]: return b
-
 
     from itertools import combinations_with_replacement # 중복조합 사용
     from collections import Counter
@@ -15,37 +12,24 @@ def solution(n, info):
     max_diff = 0
     remember = [0] * 11 # return할 배열 초기값
 
-    for i in combinations_with_replacement(score, n): # 모든 조합을 다 확인할 것임
-    
+    for i in combinations_with_replacement(score, n): # 모든 조합을 다 확인
         a = Counter(i)
-        ryan_info = [0] * 11 # 라이언 점수 초기화
+        ryan_info = [a[j] if j in a.keys() else 0 for j in range(11)] # 라이언의 기록
 
-        # 이번 iter에서 만들어진 조합을 라이언 점수 리스트에 기록
-        for j in range(11):
-            if j in a.keys():
-                ryan_info[10 - j] = a[j]
-        
-        # 점수 계산 시작
-        app_score, ryan_score = 0, 0
-        for app, ryan in zip(enumerate(info), enumerate(ryan_info)):
-            if app[1] == 0 and ryan[1] == 0:
-                pass
-            else:
-                if app[1] >= ryan[1]:
-                    app_score += 10 - app[0]
-                else:
-                    ryan_score += 10 - ryan[0]
+        # 점수 계산 시작    
+        apeach_score, ryan_score = 0, 0
+        for apeach, ryan in zip(enumerate(info), enumerate(ryan_info)):
+            if apeach[1] != 0 or ryan[1] != 0:
+                if apeach[1] >= ryan[1]: apeach_score += 10 - apeach[0]
+                else: ryan_score += 10 - ryan[0]
                     
-        score_diff = max(ryan_score - app_score, 0) # 점수 차 계산
+        score_diff = max(ryan_score - apeach_score, 0) # 점수 차 계산
 
         if max_diff < score_diff: # 새로 계산한 점수차가 더 클 때
-            max_diff = score_diff 
+            max_diff = score_diff
             remember = ryan_info # 업데이트
-        
-        elif max_diff == score_diff: # 새로 계산한 점수차가 여태 계산한 점수차랑 같은 값일 때
-            remember = lower_score_comp(remember, ryan_info) # 더 낮은 점수가 많은 배열을 찾는 함수로 찾아서 저장
-    
-    if max_diff == 0: # 모든 iter가 끝나고도 0점이면
-        return [-1] # 라이언은 못 이김
 
-    return remember
+        elif max_diff == score_diff: # 새로 계산한 점수차이가 최대점수차이랑 같을때
+            remember = a_or_b(remember, ryan_info) # 조건에 더 부합하는 배열을 저장
+    
+    return remember if max_diff != 0 else [-1]
