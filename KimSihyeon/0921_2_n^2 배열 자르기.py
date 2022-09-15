f710da3f8bd0a70ff755d4f207b80addc387fe1b
tlgus626 <a01097190079@gmail.com>
2022-09-15 11:18:33 +0900

    upload

---
+++ KimSihyeon/0921_2_n^2 배열 자르기.py
@@ -0,0 +1,35 @@
+def solution(n, left, right):
+    answer = []
+    for i in range(left, right + 1):
+        answer.append(max(divmod(i, n)) + 1)
+    return answer
+
+
+################### I want to know why is it wrong ###################
+
+def myftn(n, x):
+    res = [0] * n
+    for j in range(n):
+        if j <= x:
+            res[j] = x + 1
+        else:
+            res[j] = res[j - 1] + 1
+    return res
+
+
+def solution(n, left, right):
+    sx, sy = divmod(left, n)
+    ex, ey = divmod(right, n)
+    answer = []
+    # start
+    start = myftn(n, sx)
+    for i in range(sy, n):
+        answer.append(start[i])
+    # middle
+    for i in range(sx + 1, ex):
+        answer.extend(myftn(n, i))
+    # end
+    end = myftn(n, ex)
+    for i in range(ey + 1):
+        answer.append(end[i])
+    return answer
