785101d4eec2774381f7d638e2223873e54d015b
thon <stat.thon@gmail.com>
2022-10-17 14:57:01 +0900

    update py

--- SonDonghyun/1019_Study/Lv2_다리를-지나는-트럭.py
+++ SonDonghyun/1019_Study/Lv2_다리를-지나는-트럭.py
@@ -2,19 +2,24 @@
 from collections import deque
 
 def solution(bridge_length, weight, truck_weights):
-    queue = deque([0]*bridge_length)
-    orders = deque(truck_weights)
-    time=0
-    total=0
-    while orders:
-        time+=1
-        total -= queue[0]
-        queue.popleft()
-        if total + orders[0] > weight:
-            queue.append(0)
-        else:
-            w = orders.popleft()
-            total+=w
-            queue.append(w)
 
-    return time+bridge_length
+    on_bridge = deque([0] * bridge_length)
+    waiting = deque(truck_weights)
+    
+    time = 0
+    total = 0 # 다리 위에 남아있는 트럭의 무게 합
+
+    while waiting:
+
+        time += 1
+        total -= on_bridge[0] # 나갈 트럭이니까 total에서 무게 빼줌
+        on_bridge.popleft()
+
+        if total + waiting[0] > weight:
+            on_bridge.append(0)
+        else:
+            truck = waiting.popleft()
+            total += truck # 다링 위에 새로 온 트럭 무게 더해줌
+            on_bridge.append(truck)
+
+    return time + bridge_length
